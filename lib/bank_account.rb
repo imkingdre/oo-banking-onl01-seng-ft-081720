1e9f35a5e57031c22124ebc5ddce97148c087605
Andre King <andreking1031@gmail.com>
2020-09-01 18:04:49 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,12 @@
 class BankAccount
-  attr_reader :name, :balance, :status
+  attr_reader :name, :balance, :status, :display_balance
   
   def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
   end
+  
   
   def display_balance
     return "Your balance is $#{name.balance}"
